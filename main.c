17fc36d936d784f34bd047da6d4013c0a260d920
abdalrzag eisa <bmjfdrh@gmail.com>
2023-08-04 16:49:20 +0300

    render and move the snake

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
 /* #include <SDL2/SDL2_gfxPrimitives.h> */
+#include "lib/snake.h"
 
 const int WIDTH = 800;
 const int HEIGHT = 800;
@@ -16,10 +18,23 @@ int main(void) {
     window = SDL_CreateWindow("Csnake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, 0);
 
+    struct Snake snake;
+
+    init_game(&snake, WIDTH, HEIGHT);
+
     SDL_Event event;
     while (!quit) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+
+        render_snake(renderer, &snake);
+        handle_movement(&snake);
+
+        if (check_collisions(&snake, WIDTH, HEIGHT)) {
+            free(snake.tail);
+            break;
+        }
+
         SDL_RenderPresent(renderer);
 
         while (SDL_PollEvent(&event)) {
@@ -28,7 +43,38 @@ int main(void) {
             if (event.type == SDL_QUIT || state[SDL_SCANCODE_Q]) {
                 quit = 1;
             }
+
+            if (state[SDL_SCANCODE_UP] && snake.direction != DIRECTION_DOWN) {
+                snake.direction = DIRECTION_UP;
+            }
+
+            else if (state[SDL_SCANCODE_DOWN] && snake.direction != DIRECTION_UP) {
+                snake.direction = DIRECTION_DOWN;
+            }
+
+            else if (state[SDL_SCANCODE_LEFT] && snake.direction != DIRECTION_RIGHT) {
+                snake.direction = DIRECTION_LEFT;
+            }
+
+            else if (state[SDL_SCANCODE_RIGHT] && snake.direction != DIRECTION_LEFT) {
+                snake.direction = DIRECTION_RIGHT;
+            }
+            else if (state[SDL_SCANCODE_E]) {
+                snake.size += 1;
+                struct Tail *temp = realloc(snake.tail, (snake.size * sizeof(struct Tail)));
+
+                if (temp != NULL) {
+                    snake.tail = temp;
+                    struct Tail new_tail = {snake.pos.x, snake.pos.y};
+                    snake.tail[snake.size-1] = new_tail;
+                    printf("SUCCESS\n");
+                } else {
+                    printf("Failed to add tail\n");
+                }
+            }
         }
+
+
         SDL_Delay(120);
     }
 
